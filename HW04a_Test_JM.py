4d477b4bd7ac87a20533057275172351aa721d52
JMasekar <57605443+JMasekar@users.noreply.github.com>
2020-02-18 00:02:56 -0500

    Add files via upload

---
+++ HW04a_Test_JM.py
@@ -0,0 +1,22 @@
+"""
+@author- Jigar Masekar
+
+HW04a Testing
+
+"""
+
+import unittest
+
+from HW04a_JM import rep_checker
+
+
+class TestRep(unittest.TestCase):
+
+    def test_rep(self):
+        repos = rep_checker('JMasekar')
+        expect = [['helloworld', 1], ['stevens_ssw810', 4], ['Triangle567', 8], ['Triangles', 2]]
+        self.assertEqual(repos, expect)
+
+
+if __name__ == '__main__':
+    unittest.main(exit=False, verbosity=2)
