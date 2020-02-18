d29e509ca112eb3b837c08c7c61c5ee1d845338c
JMasekar <57605443+JMasekar@users.noreply.github.com>
2020-02-18 00:16:48 -0500

    Update HW04a_Test_JM.py

--- HW04a_Test_JM.py
+++ HW04a_Test_JM.py
@@ -14,7 +14,7 @@ class TestRep(unittest.TestCase):
 
     def test_rep(self):
         repos = rep_checker('JMasekar')
-        expect = [['helloworld', 1], ['RepChecker', 6], ['stevens_ssw810', 4], ['Triangle567', 8], ['Triangles', 2]]
+        expect = [['helloworld', 1], ['RepChecker', 8], ['stevens_ssw810', 4], ['Triangle567', 8], ['Triangles', 2]]
         self.assertEqual(repos, expect)
 
 
