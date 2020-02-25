1d4e4a64376398df766a3ad6f8aaed7af076ea9e
JMasekar <57605443+JMasekar@users.noreply.github.com>
2020-02-25 13:20:15 -0500

    HW05 changes

--- HW04a_JM.py
+++ HW04a_JM.py
@@ -5,11 +5,12 @@ Code for HW04a
 
 """
 
-import requests
 import json
+import requests
 
 
 def rep_checker(user_id):
+    """ function to check data on the repositories """
     url = 'https://api.github.com/users/'+user_id+'/repos'
     html = requests.get(url)
     loaded_json = json.loads(html.text)
@@ -30,12 +31,14 @@ def rep_checker(user_id):
 
 
 def result(repos):
+    """ function to print the results """
     for i in repos:
         print(f"Repo: {i[0]} Number of commits: {i[1]}")
     print(repos)
 
 
 def main():
+    """ function to interact with users """
     user_id = input("Enter GitHub user ID: ")
     repos = rep_checker(user_id)
     result(repos)
